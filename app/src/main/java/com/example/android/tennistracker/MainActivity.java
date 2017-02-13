b34e0395b43fea2bbc5d530de28c26e27e21229d
Andreas <aborge94@hotmail.com>
2017-02-14 00:06:06 +0100

    remove stuff

--- app/src/main/java/com/example/android/tennistracker/MainActivity.java
+++ app/src/main/java/com/example/android/tennistracker/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.android.tennistracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
